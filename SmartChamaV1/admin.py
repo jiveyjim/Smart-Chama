1968862c47350096c2e594e4b84b0deb43696020
benedict onyango <benedictonyango0@gmail.com>
2025-12-06 17:23:09 +0300

    created databes

--- SmartChamaV1/admin.py
+++ SmartChamaV1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ChamaMember
 
 # Register your models here.
+admin.site.register(ChamaMember)
+
--- SmartChamaV1/models.py
+++ SmartChamaV1/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ChamaMember(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    idNumber = models.CharField(max_length=20, unique=True)
+    joined_date = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=128)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+
+
+    def __str__(self):
+        return self.full_name
