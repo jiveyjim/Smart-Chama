a1bf8e6a69e1d3520036f903762ec6140590a207
James Makori <officialjamesmakori@gmail.com>
2025-12-08 01:29:40 -0800

    pages update

--- SmartChamaV1/urls.py
+++ SmartChamaV1/urls.py
@@ -1,10 +1,23 @@
-from django.contrib import admin
 from django.urls import path, include
 from SmartChamaV1 import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('forget password/',views.forget_password, name='forget password'),
+    path('base/', views.base, name='base'),
+    path('deposit/', views.deposit, name='deposit'),
+    path('', views.index, name='index'),
+    path('member_home/', views.member_home, name='member_home'),
+    path('member_list/', views.member_list, name='member_list'),
+    path('statements/', views.statements, name='statements'),
+    path('withdraw/', views.withdraw, name='withdraw'),
+    path('admin_announcement/', views.admin_announcement, name='admin_announcement'),
+    path('admin_email/', views.admin_email, name='admin_email'),
+    path('admin_home/', views.admin_home, name='admin_home'),
+    path('admin_login/', views.admin_login, name='admin_login'),
+    path('admin_members/', views.admin_members, name='admin_members'),
+    path('withdrawal_timeline/', views.withdrawal_timeline, name='withdrawal_timeline'),
+
+
 ]
--- SmartChamaV1/views.py
+++ SmartChamaV1/views.py
@@ -3,7 +3,51 @@ from django.http import HttpResponse
 
 def signup(request):
     return render(request, 'signup.html')
+
 def login(request):
     return render(request, 'login.html')
+
 def forget_password(request):
     return render(request, 'forget password.html')
+
+def base(request):
+    return render(request, 'base.html')
+
+def deposit(request):
+    return render(request, 'deposit.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def member_home(request):
+    return render(request, 'member_home.html')
+
+def member_list(crequest):
+    return render(request, 'member_list.html')
+
+def statements(request):
+    return render(request, 'statements.html')
+
+def withdraw(request):
+    return render(request, 'withdraw.html')
+
+def admin_announcement(request):
+    return render(request, 'admin_announcement.html')
+
+def admin_email(request):
+    return render(request, 'admin_email.html')
+
+def admin_home(request):
+    return render(request, 'admin_home.html')
+
+def admin_login(request):
+    return render(request, 'admin_login.html')
+
+def admin_members(request):
+    return render(request, 'admin_members.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def withdrawal_timeline(request):
+    return render(request, 'withdrawal_timeline.html')
